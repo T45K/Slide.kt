405e72a3bb402b295b885a35ccf2e5d607c32ff0
T45K <tasktas9@gmail.com>
2024-06-30 23:57:55 +0900

    fix ratio

--- src/main/kotlin/io/github/t45k/kmp_trial/api/PresentationOption.kt
+++ src/main/kotlin/io/github/t45k/kmp_trial/api/PresentationOption.kt
@@ -1,7 +1,8 @@
 package io.github.t45k.kmp_trial.api
 
 data class PresentationOption(
-    val animation: Animation = Animation.FROM_RIGHT_TO_LEFT
+    val animation: Animation = Animation.FROM_RIGHT_TO_LEFT,
+    val aspectRatio: AspectRatio = AspectRatio.RATIO_16_9,
 )
 
 enum class Animation {
--- src/main/kotlin/io/github/t45k/kmp_trial/engine/PresentationEngine.kt
+++ src/main/kotlin/io/github/t45k/kmp_trial/engine/PresentationEngine.kt
@@ -1,19 +1,23 @@
 package io.github.t45k.kmp_trial.engine
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,8 +29,11 @@ import io.github.t45k.kmp_trial.api.Vertical
 fun handlePresentation(presentation: Presentation, option: PresentationOption) = application {
     val navController = rememberNavController()
     val slideTransition = slideTransition(option.animation)
+    val windowState = rememberWindowState()
+
     Window(
         onCloseRequest = ::exitApplication,
+        state = windowState,
         title = "Slide.kt",
         onKeyEvent = moveSlideEvent(
             { navController.currentDestination!!.route!!.toInt() },
@@ -44,39 +51,49 @@ fun handlePresentation(presentation: Presentation, option: PresentationOption) =
                     popEnterTransition = { slideTransition.popEnter },
                     popExitTransition = { slideTransition.popExit }
                 ) {
-                    Column(Modifier.fillMaxSize().padding(horizontal = 12.dp, vertical = 12.dp)) {
-                        Box(
-                            Modifier.fillMaxWidth(),
-                            contentAlignment = when (slide.headerPosition) {
-                                Horizontal.LEFT -> Alignment.CenterStart
-                                Horizontal.CENTER -> Alignment.Center
-                            }
-                        ) {
-                            Text(
-                                text = slide.title,
-                                fontSize = 4.em,
-                                fontWeight = FontWeight.Bold,
-                            )
-                        }
-
-                        Box(Modifier.fillMaxWidth().padding(vertical = 4.dp))
-
+                    Box(Modifier.fillMaxSize().background(Color.Black)) {
                         Column(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalArrangement = when (slide.paragraphPosition.second) {
-                                Vertical.TOP -> Arrangement.Top
-                                Vertical.CENTER -> Arrangement.Center
-                            },
-                            horizontalAlignment = when (slide.paragraphPosition.first) {
-                                Horizontal.LEFT -> Alignment.Start
-                                Horizontal.CENTER -> Alignment.CenterHorizontally
-                            },
-                        ) {
-                            slide.paragraphs.forEach { parText ->
-                                Text(
-                                    text = parText,
-                                    fontSize = 3.em,
+                            Modifier.fillMaxSize()
+                                .aspectRatio(
+                                    option.aspectRatio.ratio,
+                                    (windowState.size.width) / (windowState.size.height) > option.aspectRatio.ratio
                                 )
+                                .background(Color.White)
+                                .padding(horizontal = 12.dp, vertical = 12.dp)
+                        ) {
+                            Box(
+                                Modifier.fillMaxWidth(),
+                                contentAlignment = when (slide.headerPosition) {
+                                    Horizontal.LEFT -> Alignment.CenterStart
+                                    Horizontal.CENTER -> Alignment.Center
+                                }
+                            ) {
+                                Text(
+                                    text = slide.title,
+                                    fontSize = 4.em,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                            }
+
+                            Box(Modifier.fillMaxWidth().padding(vertical = 4.dp))
+
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = when (slide.paragraphPosition.second) {
+                                    Vertical.TOP -> Arrangement.Top
+                                    Vertical.CENTER -> Arrangement.Center
+                                },
+                                horizontalAlignment = when (slide.paragraphPosition.first) {
+                                    Horizontal.LEFT -> Alignment.Start
+                                    Horizontal.CENTER -> Alignment.CenterHorizontally
+                                },
+                            ) {
+                                slide.paragraphs.forEach { parText ->
+                                    Text(
+                                        text = parText,
+                                        fontSize = 3.em,
+                                    )
+                                }
                             }
                         }
                     }
