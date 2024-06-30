6a4a2ed1880d77b4247ec2d4c6797223f0cefc18
T45K <tasktas9@gmail.com>
2024-06-30 18:48:47 +0900

    save key input event is captured twice...

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.androidx.navigation:navigation-compose:2.7.0-alpha07")
 }
 
 compose.desktop {
--- src/main/kotlin/io/github/t45k/kmp_trial/Main.kt
+++ src/main/kotlin/io/github/t45k/kmp_trial/Main.kt
@@ -1,106 +1,64 @@
 package io.github.t45k.kmp_trial
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.withTransform
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.delay
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-@Preview
-fun App() {
-    MaterialTheme {
-        Clock(
-            modifier = Modifier
-                .aspectRatio(1.0f)
-                .padding(100.dp)
-        )
-    }
+fun Slide(index: Int) {
+    Text("Slide $index\nNext ${index + 1}")
 }
 
-@Composable
-fun Clock(modifier: Modifier) {
-    val isStart = remember { mutableStateOf(false) }
-    val secondProgress = remember { mutableStateOf(0f) }
-    val minProgress = remember { mutableStateOf(0f) }
-
-    LaunchedEffect(isStart.value) {
-        while (isStart.value) {
-            if (secondProgress.value == 360f) {
-                secondProgress.value = 0f
-                minProgress.value += 6f
-                minProgress.value %= 360f
-            }
-            secondProgress.value += 6f
-            delay(1000)
-        }
-    }
-
-    Button(
-        onClick = { isStart.value = !isStart.value },
-        content = {
-            if (isStart.value) {
-                Text("pause")
+fun moveSlideEvent(navController: NavHostController): (KeyEvent) -> Boolean = {
+    val currentIndex = navController.currentDestination!!.route!!.toInt()
+    println(it.key)
+    when (it.key) {
+        Key.Enter, Key.DirectionRight -> {
+            if (currentIndex == 100) {
+                println("Cannot move")
             } else {
-                Text("Start")
+                navController.navigate((currentIndex + 1).toString())
             }
+            true
         }
-    )
 
-    Canvas(modifier = modifier) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-        drawCircle(
-            color = Color.Black,
-            center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-            radius = size.minDimension / 2,
-            style = Stroke(5F)
-        )
-        withTransform(
-            { rotate(secondProgress.value) },
-            {
-                drawLine(
-                    strokeWidth = 4.dp.toPx(),
-                    cap = StrokeCap.Round,
-                    color = Color.Blue,
-                    start = center,
-                    end = Offset(size.minDimension / 2, 12.dp.toPx())
-                )
+        Key.DirectionLeft -> {
+            if (currentIndex == 1) {
+                println("Cannot move")
+            } else {
+                navController.navigate((currentIndex - 1).toString())
             }
-        )
-        withTransform(
-            { rotate(minProgress.value) },
-            {
-                drawLine(
-                    strokeWidth = 4.dp.toPx(),
-                    cap = StrokeCap.Round,
-                    color = Color.Blue,
-                    start = center,
-                    end = Offset(size.minDimension / 2, 100.dp.toPx())
-                )
-            }
-        )
+            true
+        }
+
+        else -> false
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "My first app") {
-        App()
+    val navController = rememberNavController()
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "My first app",
+        onKeyEvent = moveSlideEvent(navController),
+    ) {
+        Scaffold {
+            NavHost(navController, startDestination = "1") {
+                (1..100).forEach { index ->
+                    composable(index.toString()) {
+                        Slide(index)
+                    }
+                }
+            }
+        }
     }
 }
